799395f0db844dc486642a4c020fa92c7bfc1f6b
haesol Jang <wkdgothf@gmail.com>
2022-11-08 00:53:17 +0900

    setup

---
+++ src/main/java/kr/legossol/sampleKeycloak/SampleKeycloakApplication.java
@@ -0,0 +1,13 @@
+package kr.legossol.sampleKeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/legossol/sampleKeycloak/SampleKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.legossol.sampleKeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
